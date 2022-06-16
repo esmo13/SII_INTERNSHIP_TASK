cf4e0b2bc8d656df13ebf698972cc88dbaf44625
esmo13 <skalski.szymon123@gmail.com>
2022-06-16 22:20:32 +0200

    remove unnecessary files

--- src/main/java/com/example/demo/Exceptions/LessonFullException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Exceptions;
-
-public class LessonFullException extends RuntimeException{
-    public LessonFullException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/demo/Exceptions/LoginTakenException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Exceptions;
-
-public class LoginTakenException extends RuntimeException{
-    public LoginTakenException(String message) {
-        super(message);
-    }
-}
